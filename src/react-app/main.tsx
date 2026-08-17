9a563e1bd0da95a02cb8f2df5b7bb628960cc166
HajimiKitty <winniringy@gmail.com>
2026-08-17 18:19:34 +0800

    load About brand styles

--- src/react-app/main.tsx
+++ src/react-app/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import "./content.css";
 import "./guide.css";
+import "./about.css";
 import App from "./App.tsx";
 import { LanguageProvider } from "./lib/LanguageProvider";
 import { ThemeProvider } from "./lib/ThemeProvider";
